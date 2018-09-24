434b6b848ab261bd35b5ee8efcf1db89ea558357
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-24 23:49:33 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,9 +1,30 @@
+require 'pry'
 class PigLatinizer
-  attr_accessor :phrase
 
-  def initialize(phrase)
-    @phrase = phrase.downcase.split(" ")
+  # def initialize
+  #
+  # end
+
+  def piglatinize(phrase)
+    words = phrase.split(" ")
+    phr = words.map do |word|
+      w = word.split('')
+      if w.first == (/[aeiouAEIOU]/)
+        w << "way"
+      else
+        w.each do |char|
+
+          finished_w = []
+          if char != (/[aeiouAEIOU]/)
+            char << finished_w
+          else
+            finished_w << 'ay'
+            return finished_w.join
+          end
+        end
+      end
+      w.join
+    end
+    return phr.join(" ")
   end
-
-
 end
