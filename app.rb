2ef538bef7611ca1fc2810c1f7cadf21a875d9ec
Przemyslaw Dabrowski <bluh@outlook.com>
2018-09-25 01:12:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,4 +4,10 @@ class App < Sinatra::Base
   get '/' do
     erb :user_input
   end
+
+  post '/piglatinize' do
+    @latinized_text = PigLatinizer.new.piglatinize(params[:user_phrase])
+
+    erb :piglatinize
+  end
 end
